23b21bbaf46ec96b2f0268ff10d471eea8d1a160
Medhi Sam <sammedhi2@hotmail.fr>
2018-10-13 06:47:18 -0400

    variables histogram used for conv and fc visualization

--- utilNN.py
+++ utilNN.py
@@ -7,13 +7,15 @@ def fc_layer(input , size , act_f=None):
         act = tf.add(tf.matmul(input , W) , B)
     else:
         act = act_f(tf.add(tf.matmul(input , W) , B) )
+    tf.summary.histogram("W" , W)
+    tf.summary.histogram("act" , act)
     return act , W , B
 
 
 def conv(input ,  kernel_size , nb_kernel , in_channel , act=tf.nn.relu , strides = [0 , 1 , 1 , 0]):
     W = tf.get_variable("Wconv" , [kernel_size , kernel_size , in_channel , nb_kernel] , tf.float32 , tf.contrib.layers.xavier_initializer())
     B = tf.get_variable("Bconv" , [nb_kernel] , tf.float32 , tf.constant_initializer(0.0))
-
+    tf.summary.histogram("W" , W)
     return act(tf.add ( tf.nn.conv2d( input , W , strides , "SAME") , B) )
 
 def max_pool2x2(input):
